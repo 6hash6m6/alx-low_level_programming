b4eba4efcc71b395e7ba9f16787752a94e878b01
Hashim <ahmedhashim.rn@gmail.com>
2023-03-17 12:08:43 -0700

    pc5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,24 @@
 #include <stdio.h>
-
-/**
- * main - prints all possible combinations of two two-digit numbers
- * from 00-99, with no repeats and separated by commas and spaces.
- * All numbers are printed with two digits.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    int tens1, tens2, ones1, ones2;
+    int tens, ones, tens2, ones2;
 
-    for (tens1 = 0; tens1 <= 9; tens1++) /* print first two-digit combo */
+    for (tens = 0; tens <= 9; tens++)
     {
-        for (ones1 = 0; ones1 <= 9; ones1++)
+        for (ones = 0; ones <= 9; ones++)
         {
-            for (tens2 = tens1; tens2 <= 9; tens2++) /* print second two-digit combo */
+            for (tens2 = tens; tens2 <= 9; tens2++)
             {
-                for (ones2 = ones1; ones2 <= 9; ones2++)
+                for (ones2 = ones + 1; ones2 <= 9; ones2++)
                 {
-                    if (tens1 == tens2 && ones2 <= ones1) /* no repeat combinations */
-                        continue;
-
-                    putchar(tens1 + '0');
-                    putchar(ones1 + '0');
+                    putchar(tens + '0');
+                    putchar(ones + '0');
                     putchar(' ');
                     putchar(tens2 + '0');
                     putchar(ones2 + '0');
 
-                    if (tens1 == 9 && ones1 == 8 && tens2 == 9 && ones2 == 9)
-                        putchar('\n');
-                    else
+                    if (!(tens == 9 && ones == 8 &&
+                          tens2 == 9 && ones2 == 9))
                     {
                         putchar(',');
                         putchar(' ');
@@ -39,6 +27,7 @@ int main(void)
             }
         }
     }
+    putchar('\n');
 
     return (0);
 }
