e4bf486f3ed58d01887f03486eddf94a86a83c0d
Hashim <ahmedhashim.rn@gmail.com>
2023-04-04 22:43:02 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
-include "main.h"
+#include "main.h"
 
-int t_prime(int n, int i);
+int temp_prime(int n, int i);
 
 /**
  * is_prime_number - integer is a prime num or not
@@ -8,25 +8,25 @@ int t_prime(int n, int i);
  * Return: ? 1 : 0
  */
 
-int is_prime_numner(int n)
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (t_prime(n, n - 1));
+	return (temp_prime(n, n - 1));
 }
 
 /**
- * t_prime - calc num is R
+ * temp_prime - calc num is R
  * @n: n
  * @i: i
  * Return: ? 1 : 0
  */
 
-int t_prime(int n, int i)
+int temp_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (t_prime(n, i - 1));
+	return (temp_prime(n, i - 1));
 }
