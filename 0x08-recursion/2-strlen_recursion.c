c139c56e450ed65a0673f8e49dfa800092fd7a29
Hashim <ahmedhashim.rn@gmail.com>
2023-04-04 22:05:26 -0700

    fixing betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,8 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length
- *
- * return the length of the string
+ * return: the length of the string
  */
 
 int _strlen_recursion(char *s)
