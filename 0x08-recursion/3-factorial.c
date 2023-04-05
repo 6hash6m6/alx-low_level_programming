544372d2745f527ed479899e2b3a8fbeb7dc95f6
Hashim <ahmedhashim.rn@gmail.com>
2023-04-04 22:07:26 -0700

    fixing betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,14 @@
 /**
  * factorial - return the factoria of a number
  * @n: number to return the factorial from
- * return: factorial of n
+ * Return: factorial of n
  */
 
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n ==0)
+	if (n == 0)
 		return (1);
-	return (n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
