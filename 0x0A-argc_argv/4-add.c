0938c9170420c25b0e36144e50f949324bee53c0
Hashim <ahmedhashim.rn@gmail.com>
2023-04-09 23:55:30 -0700

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main func
+ * @argc: int
+ * @argv: char
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, res, s = 0;
+	char c[] = "Error", *find_letter;
+
+	if (argc > )
+	{
+		for (i = 1; i < argc; i++)
+		{
+			find_letter = argv[i];
+			while (*find_letter != 0)
+			{
+				if (*find_letter < 47 || *find_letter > 57)
+				{
+					printf("%s\n", c);
+					return (1);
+				}
+				find_letter++;
+			}
+			res = atoi(argv[i]);
+			s += res;
+		}
+		printf("%d\n", s);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
+
