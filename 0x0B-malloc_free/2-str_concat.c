a4617f239f70080382ec1d9e0bd0da15e2eef4dd
Hashim <ahmedhashim.rn@gmail.com>
2023-04-11 16:22:36 -0700

    fixing condition

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,12 +16,14 @@ char *str_concat(char *s1, char *s2)
 	int n1, n2, count;
 
 	count = 0;
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
-
 	for (n1 = 0; s1[n1] != '\0'; n1++)
 		;
 	for (n2 = 0; s2[n2] != '\0'; n2++)
