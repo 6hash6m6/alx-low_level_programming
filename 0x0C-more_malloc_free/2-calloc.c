baca305a6f91bc63cac33c39020c236961bad9a2
Hashim <ahmedhashim.rn@gmail.com>
2023-04-13 11:47:44 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _Calloc - ep
+ * @nmemb: uint
+ * @size: uint
+ * Return: p;
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int x, y;
+
+	y = nmemb * size;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	p = malloc(y);
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0; x < (d); x++)
+	{
+		p[x] = 0;
+	}
+
+	return (p);
+}
