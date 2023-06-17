30406bc6f463fdaf9f6903362076f3b7acd533a8
Hashim <ahmedhashim.rn@gmail.com>
2023-06-17 15:20:36 -0700

    fixed return empty str if null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *strConc;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
