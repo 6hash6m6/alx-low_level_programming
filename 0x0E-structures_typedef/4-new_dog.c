e272886240074a371261604ea010b139e4c9cc87
Hashim <ahmedhashim.rn@gmail.com>
2023-06-20 15:39:58 -0700

    fixing output

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,53 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * new_dog - creates new dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
- * Return: p;
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: p
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+    dog_t *p;
+    int name_len, owner_len;
+
+    /* Reserve memory for struct */
+    p = malloc(sizeof(dog_t));
+    if (p == NULL)
+        return (NULL);
+
+    /* Check if name or owner is NULL */
+    if (name == NULL || owner == NULL)
+    {
+        free(p);
+        return (NULL);
+    }
+
+    /* Calculate string lengths */
+    name_len = strlen(name);
+    owner_len = strlen(owner);
+
+    /* Allocate memory for name and owner strings */
+    p->name = malloc((name_len + 1) * sizeof(char));
+    p->owner = malloc((owner_len + 1) * sizeof(char));
+    if (p->name == NULL || p->owner == NULL)
+    {
+        free(p->name);
+        free(p->owner);
+        free(p);
+        return (NULL);
+    }
+
+    /* Copy name and owner strings */
+    strcpy(p->name, name);
+    strcpy(p->owner, owner);
+
+    /* Set age */
+    p->age = age;
+
+    return (p);
 }
+
